ba107e4537430fb9ac45b070899b3edb189738c0
nmowens95 <nmowens95@gmail.com>
2023-10-18 18:15:47 -0500

    Set up basic spider program

--- postscrape/postscrape/spiders/postspider.py
+++ postscrape/postscrape/spiders/postspider.py
@@ -1,1 +1,11 @@
 import scrapy
+
+class PostsSpider(scrapy.Spider):
+    name = "posts"
+    allowed_domains = "https://books.toscrape.com"
+    start_urls = [
+       "https://books.toscrape.com/index.html"
+    ]
+
+    def parse(self, response):
+        pass
