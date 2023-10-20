0e6c86cd507cc0c75ccab2e25475258721168725
nmowens95 <nmowens95@gmail.com>
2023-10-20 11:51:53 -0500

    Used css selectors to find content i was looking for, added some logic to crawl the pages

--- postscrape/postscrape/spiders/postspider.py
+++ postscrape/postscrape/spiders/postspider.py
@@ -8,4 +8,22 @@ class PostsSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        pass
+         # gets each book on the page
+         book = response.css("article.product_pod")
+
+        # we are going to loop through each book on the page obtaining our information
+         for b in book:
+            yield {
+                "name" : b.css("h3 a::text").get(),
+                "price" : b.css(".product_price .price_color::text").get(),
+                "url" : b.css(".image_container a").attrib["href"],
+            }
+
+            next_page = response.css("li.next a ::attr(href)").get()
+
+            if next_page is not None:
+                if "catalogue/" in next_page:
+                    next_page_url = "https://books.toscrape.com/" + next_page
+                else:
+                    next_page_url = "https://books.toscrape.com/catalogue/" + next_page
+            yield response.follow(next_page_url, callback = self.parse)
